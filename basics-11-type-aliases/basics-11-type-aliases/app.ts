848741ed23f41db57390f593fe84b8ab927644a1
ajzmbhc <ajzelaya@banhcafe.hn>
2025-04-04 13:57:04 -0600

    fix: add basics

--- basics-11-type-aliases/basics-11-type-aliases/app.ts
+++ basics-11-type-aliases/basics-11-type-aliases/app.ts
@@ -1,16 +1,17 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-
+type Combinable = number | string; //* type keyword is usado solo en ts, no js, se ponen los tipos posibles, union types o normales
+type ConversionDescriptor = "as-number" | "as-text"; //* literal types
+//* type User = { name: string; age: number }; //* object types
 function combine(
-  input1: Combinable,
+  // prettier-ignore
+  input1: number | string,
   input2: Combinable,
   resultConversion: ConversionDescriptor
 ) {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+  let result : Combinable;
+  if ((typeof input1 === "number" && typeof input2 === "number") || resultConversion === "as-number") {
     result = +input1 + +input2;
   } else {
-    result = input1.toString() + input2.toString();
+    result = input1.toString() + " " + input2.toString();
   }
   return result;
   // if (resultConversion === 'as-number') {
@@ -20,11 +21,11 @@ function combine(
   // }
 }
 
-const combinedAges = combine(30, 26, 'as-number');
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combinedStringAges = combine('30', '26', 'as-number');
+const combinedStringAges = combine("30", "26", "as-number");
 console.log(combinedStringAges);
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
+const combinedNames = combine("Max", "Anna", "as-text");
 console.log(combinedNames);
--- basics-11-type-aliases/basics-11-type-aliases/basics.ts
+++ basics-11-type-aliases/basics-11-type-aliases/basics.ts
@@ -1,3 +1,9 @@
+enum Role { //*number 0, n1, n2
+  ADMIN = "ADMIN", //0 //*Por defecto se ponen como numerales incrementales
+  READ_ONLY = 100, //1
+  AUTHOR = "AUTHOR", //2
+} //*Se les puede asignar en este objeto tipo enum, y se ponen los valores como =
+
 function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
   //   throw new Error('Incorrect input!');
@@ -14,6 +20,6 @@ let number1: number;
 number1 = 5;
 const number2 = 2.8;
 const printResult = true;
-let resultPhrase = 'Result is: ';
+let resultPhrase = "Result is: ";
 
 add(number1, number2, printResult, resultPhrase);
