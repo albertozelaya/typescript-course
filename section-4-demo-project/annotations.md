653efabba4ded7b27f500c3cad45b77e564b44b7
ajzmbhc <ajzelaya@banhcafe.hn>
2025-05-05 16:22:19 -0600

    feat: add first project calculator

---
+++ section-4-demo-project/annotations.md
@@ -0,0 +1,11 @@
+# Typescript Essentials Demo Project
+
+Cuando ya se tiene el _tsconfig.json_ para generar el archivo js a partir del ts, solo se usa tsc, siempre que este en el primer nivel
+
+## Ejecutar un código Js sin hacer una webPage
+
+Con Node.js es posible, solo es dirigirse a la terminal y poner el archivo Js que se quiere correr, poniendo:
+
+```
+node calculator.js
+```
--- section-4-demo-project/calculator.ts
+++ section-4-demo-project/calculator.ts
@@ -11,15 +11,64 @@ type InvestmentData = {
   duration: number;
 };
 
+type InvestmentResult = {
+  year: string;
+  totalAmount: number;
+  totalContributions: number;
+  totalInterestEarned: number;
+};
 
+type CalculationResult = InvestmentResult[] | string;
 
-function calculateInvestment(data: InvestmentData) {
-  
+function calculateInvestment(data: InvestmentData): CalculationResult {
+  const { initialAmount, annualContribution, expectedReturn, duration } = data;
+  if (initialAmount < 0) return "Initial investment amount must be at least zero";
+  if (duration <= 0) return "No valid amount of years provided";
+  if (expectedReturn <= 0) return "Expected return must be at least zero";
+
+  let total = initialAmount;
+  let totalContributions = 0;
+  let totalInterestEarned = 0;
+
+  const annualResults: InvestmentResult[] = [];
+
+  for (let i = 0; i < duration; i++) {
+    total = total * (1 + expectedReturn); //* 0.08
+    totalInterestEarned = total - totalContributions - initialAmount;
+    totalContributions = totalContributions + annualContribution;
+    total = total + annualContribution;
+
+    annualResults.push({
+      year: `Year ${i + 1}`,
+      totalAmount: total,
+      totalInterestEarned,
+      totalContributions,
+    });
+  }
+  return annualResults;
 } //* =>result[]
 
-function printResults() {
+function printResults(results: CalculationResult) {
+  if (typeof results === "string") {
+    console.log(results);
+    return;
+  }
   //* print (output) the result data
+  for (const yearEndResult of results) {
+    console.log(yearEndResult.year);
+    console.log(`Total: ${yearEndResult.totalAmount.toFixed(2)}`);
+    console.log(`Total Contributions: ${yearEndResult.totalContributions.toFixed(2)}`);
+    console.log(`Total Interest Earned: ${yearEndResult.totalInterestEarned.toFixed(2)}`);
+    console.log("-------------");
+  }
 }
 
-// const results = calculateInvestment(...)
-// printResults(results)
+const InvestmentData: InvestmentData = {
+  initialAmount: 5000,
+  annualContribution: 500,
+  expectedReturn: 0.08,
+  duration: 10,
+};
+
+const results = calculateInvestment(InvestmentData);
+printResults(results)
