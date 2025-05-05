77886510d4528f161aacd9def9284ec25d02ea83
ajzmbhc <ajzelaya@banhcafe.hn>
2025-05-05 13:26:00 -0600

    feat: create first custom type

---
+++ section-4-demo-project/calculator.ts
@@ -0,0 +1,25 @@
+//* data
+//* initial amount
+//* annual contribution
+//* expected return
+//* duration
+
+type InvestmentData = {
+  initialAmount: number;
+  annualContribution: number;
+  expectedReturn: number;
+  duration: number;
+};
+
+
+
+function calculateInvestment(data: InvestmentData) {
+  
+} //* =>result[]
+
+function printResults() {
+  //* print (output) the result data
+}
+
+// const results = calculateInvestment(...)
+// printResults(results)
